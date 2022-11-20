b063a0ceb190ba6efb51407cb1cea703500389b0
KamoEllen <108501662+KamoEllen@users.noreply.github.com>
2022-11-20 13:52:32 +0000

    Pending changes exported from your codespace

---
+++ blog01/src/App.js
@@ -0,0 +1,14 @@
+import TopBar from "./components/topbar/TopBar";
+import Settings from "./pages/settings/Settings";
+
+export default function App() {
+  return (
+    <>
+    <TopBar/>
+    <Settings/>
+    
+    </>
+  );
+}
+
+
