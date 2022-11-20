1757ecc34926ba4b14253a91658cde2c08f976ef
KamoEllen <108501662+KamoEllen@users.noreply.github.com>
2022-11-20 11:29:48 +0000

    Pending changes exported from your codespace

---
+++ blog01/src/components/SinglePost/SinglePost.jsx
@@ -0,0 +1,35 @@
+import "./singlepost.css"
+
+export default function SinglePost() {
+  return (
+    <div className='singlePost'>
+       <div className='singlePostWrapper'>
+      <img src="https://i.pinimg.com/564x/68/68/7b/68687bf74257e86caab58aafddc326a7.jpg"
+       alt="" className='singlePostImg'/>
+      <h1 className="singlePostTitle">
+        Lorem ipsum dolor sit amet
+         <div className="singlePostEdit">
+          <i className="singlePostIcon far fa-edit"></i>
+          <i className="singlePostIcon far fa-trash-alt"></i>
+         </div>
+         </h1>
+        <div className="singlePostInfo">
+          <span className="singlePostAuthor">Author: <b>Kamo</b></span>
+          <br/>
+          <span className="singlePostDate">Date: <b>19 November 2022</b></span>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed 
+        do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+         Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+         laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
+         irure dolor in reprehenderit in voluptate velit esse cillum 
+         dolore eu fugiat nulla pariatur. Excepteur sint occaecat 
+         cupidatat non proident, sunt in culpa qui officia deserunt 
+         mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+         </p>
+
+        </div>
+      </div>
+    </div>
+  )
+}
