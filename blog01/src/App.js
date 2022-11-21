cb4c401bddf69745c36ac2b85380c339a4942a71
KamoEllen <108501662+KamoEllen@users.noreply.github.com>
2022-11-21 09:04:40 +0000

    Pending changes exported from your codespace

---
+++ blog01/src/App.js
@@ -0,0 +1,40 @@
+import TopBar from "./components/topbar/TopBar";
+import Register from "./pages/register/Register";
+import Settings from "./pages/settings/Settings";
+import Write from "./pages/write/Write";
+import Single from "./pages/single/Single";
+import Login from "./pages.login/Login";
+import Home from "./components/pages/home/Home";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+}from "react-router-dom";
+import Login from "./pages/login/Login";
+
+export default function App() {
+  const user = true;
+  return (
+    <Router>
+    <TopBar/>
+    <Switch>
+      <Route exact path="/home">
+        <Home/>
+      </Route>
+
+      <Route path="/register">{ user ? <Home/> :<Register/>}</Route>
+      <Route path="/"login>{user ? <Home/> :<Login/>}</Route>
+      <Route path="/write">{user ? <Write/> :<Register/>}</Route>
+      <Route path="/settings">{user ? <Settings/> : <Register/>}<Route/>
+      <Route path="/post/:postId">
+        <Single/>
+      </Route>
+      </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+
