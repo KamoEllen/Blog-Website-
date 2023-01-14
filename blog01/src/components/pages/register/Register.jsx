16c09b57d302804e06d35cb8ac495d0536d7c9cb
KamoEllen <108501662+KamoEllen@users.noreply.github.com>
2023-01-14 23:48:55 +0000

    Pending changes exported from your codespace

---
+++ blog01/src/components/pages/register/Register.jsx
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom';
+import './register.css'
+
+export default function Register() {
+  return (
+    <div className="register">
+        <span className="registerTitle">Register</span>
+      <form className='registerForm'>
+      <label>Username</label>
+        <input type="text" placeholder="Enter your username.."/>
+        <label>Email</label>
+        <input type="text" placeholder="Enter your email.."/>
+        <label>Password</label>
+        <input type="password" className="registerInput" placeholder="Enter your password.."/>
+        <button className="loginRegisterButton">Register</button>
+      </form>
+      <button className="loginButton">Login</button>
+
+
+     <Link className="link" to="/login">LOGIN</Link>
+
+
+       </div>
+  );
+}
